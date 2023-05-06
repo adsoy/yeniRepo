bb66ce029e54f78e22a66fbc04c563002b91e4cd
Adnan Söylemez <dnnsylmz78@gmail.com>
2023-05-06 21:40:19 +0300

    V1

---
+++ src/main/java/Class01.java
@@ -0,0 +1,12 @@
+public class Class01 {
+    public static void main(String[] args) {
+
+        //V1 çalışmalarım
+        int a= 5;
+        int b=7;
+        System.out.println(a*b);
+
+
+
+    }
+}
